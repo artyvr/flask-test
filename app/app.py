b2ed96fd3f4f6569426df4f125a607639da06db0
art <artyvr@yandex.ru>
2023-11-09 23:44:47 +0300

    Add get remote ip

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    remote_ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    return render_template('index.html', remote_ip=remote_ip)
 
 @app.route("/about")
 def about():
