32d79e5e511829581e71657e12a1cd5bbf7bdcc4
art <artyvr@yandex.ru>
2023-11-11 23:02:32 +0300

    add random page

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template
+import random
+import string
+import secrets
 
 app = Flask(__name__)
 
@@ -10,3 +13,9 @@ def index():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/random")
+def random():
+    characters = string.ascii_letters + string.digits + string.punctuation
+    random_characters = ''.join(secrets.choice(characters) for i in range(16))
+    return render_template('random.html', random_characters=random_characters)
--- app/app_test.py
+++ app/app_test.py
@@ -12,6 +12,11 @@ class BasicTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/about', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+    
+    def test_random(self):
+        tester = app.test_client(self)
+        response = tester.get('/random', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
   unittest.main()
