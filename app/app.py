89bc3c2233c8da271588857c5cee6215ee626796
art <artyvr@yandex.ru>
2023-11-12 13:41:08 +0300

    lint

--- app/app.py
+++ app/app.py
@@ -1,21 +1,23 @@
-from flask import Flask, request, render_template
-import random
 import string
 import secrets
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ Index page """
     remote_ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     return render_template('index.html', remote_ip=remote_ip)
 
 @app.route("/about")
 def about():
+    """ About page """
     return render_template('about.html')
 
 @app.route("/random")
 def random():
+    """ Random Page"""
     characters = string.ascii_letters + string.digits + string.punctuation
     random_characters = ''.join(secrets.choice(characters) for i in range(16))
     return render_template('random.html', random_characters=random_characters)
--- app/app_test.py
+++ app/app_test.py
@@ -1,22 +1,27 @@
-from app import app
+""" Application tests """
+
 import unittest
+from app import app
 
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
+        """ Test index page """
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     def test_about(self):
+        """ Test about page """
         tester = app.test_client(self)
         response = tester.get('/about', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_random(self):
+        """ Test random page """
         tester = app.test_client(self)
         response = tester.get('/random', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
